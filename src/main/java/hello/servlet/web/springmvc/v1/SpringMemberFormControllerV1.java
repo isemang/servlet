67ef72fc20110c2532547d1da74e84c9ab7bdfc9
isemang <a55306039@gmail.com>
2022-03-18 11:33:45 +0900

    feat - mvc 시작, 회원등록 폼 완성

---
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -0,0 +1,14 @@
+package hello.servlet.web.springmvc.v1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller     //@Component와 @RequestMapping이 붙어있는것과 동일한 효과를 낸다
+public class SpringMemberFormControllerV1 {
+
+    @RequestMapping("/springmvc/v1/members/new-form")
+    public ModelAndView process() {
+        return new ModelAndView("new-form");
+    }
+}
